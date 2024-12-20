362fcd767538a935f16c23bd451589c7c8752f79
hotwellkz <hotwell.kz@gmail.com>
2024-12-20 11:47:21 +0600

    Initial commit

---
+++ src/lib/firebase-config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDlXpMKumjFJCcYRQaPgmdftnDAQSND9Os",
+  authDomain: "bai001.firebaseapp.com",
+  projectId: "bai001",
+  storageBucket: "bai001.firebasestorage.app",
+  messagingSenderId: "934824698688",
+  appId: "1:934824698688:web:306bcb1b9bdf746d8610a5"
+};
